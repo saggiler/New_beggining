1a51465d2c6f5ac54bcb5aab6417a4d0fb552e79
Jack <saggiler@gmail.com>
2025-12-20 20:55:57 +0300

    feat: код перенесён из main в отдельные классы и методы, сервер переписан с socket на HttpServer, появилась возможность множественных запросов ,

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,52 +1,12 @@
 package org.example;
 
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        try {
-            System.out.println("Запуск сервера");
+        MyServer myServer = new MyServer();
+        MyHandler myHandler = new MyHandler();
 
-            ServerSocket serverSocket = new ServerSocket(8081);
-            System.out.println("Сервер запущен на порту 8081");
-
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Клиент подключился");
-
-            InputStream inputStream = clientSocket.getInputStream();
-            InputStreamReader reader = new InputStreamReader(inputStream);
-
-            BufferedReader bufferedReader = new BufferedReader(reader);
-
-            String thisLine = bufferedReader.readLine();
-
-            System.out.println("Это " + thisLine);
-
-            OutputStream outputStream = clientSocket.getOutputStream();
-
-            if (thisLine != null && thisLine.startsWith("GET /ping")) {
-
-                String answer = "pong";
-                String httpResponse =
-                        "HTTP/1.1 200 OK\r\n" +
-                                "Content-Type: text; charset=UTF-8\r\n" +
-                                "Content-Length: " + answer.length() + "\r\n" +
-                                "\r\n" +
-                                answer;
-
-                outputStream.write(httpResponse.getBytes());
-                outputStream.flush();
-            }
-
-            clientSocket.close();
-            serverSocket.close();
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        myServer.start(myHandler);
     }
 }
---
+++ src/main/java/org/example/MyHandler.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MyHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String response = "pong";
+        exchange.sendResponseHeaders(200, response.length());
+
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(response.getBytes());
+        }
+    }
+}
